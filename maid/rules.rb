55d540e288f3b468fb8bd3d51794d0ffc7a00a5c
Sho DOUHASHI <douhashi@gmail.com>
2019-03-24 02:30:16 +0900

    update

--- maid/rules.rb
+++ maid/rules.rb
@@ -5,13 +5,13 @@ Maid.rules do
   repeat '3600s' do
     rule 'Trash downloads' do
       dir('~/Downloads/*').each do |path|
-        trash(path) if 1.month.since?(accessed_at(path))
+        trash(path) if 4.weeks.since?(accessed_at(path))
       end
     end
 
     rule 'Trash maid.log' do
       dir('~/.maid/*.log').each do |path|
-        trash(path) if 1.week.since?(created_at(path))
+        trash(path) if 1.weeks.since?(created_at(path))
       end
     end
   end
@@ -21,7 +21,7 @@ Maid.rules do
     rule 'Move videos' do
       video_root = '~/Videos/.private'
 
-      dir('~/Downloads/*.{mp4}').each do |path|
+      dir('~/Downloads/*.{mp4,avi,mpg}').each do |path|
         dirname = Date.today.strftime('%Y%m')
         dest_path = File.join(video_root, 'downloads', dirname)
         mkdir(dest_path)
